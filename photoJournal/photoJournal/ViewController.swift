d9c636108ddad10fabcca11d924db0efceb55d55
atj2097 <52180742+atj2097@users.noreply.github.com>
2019-10-04 14:56:02 -0400

    Add files via upload

---
+++ photoJournal/photoJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photoJournal
+//
+//  Created by God on 10/4/19.
+//  Copyright © 2019 God. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
